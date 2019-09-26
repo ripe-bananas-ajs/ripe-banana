ce4aeb6abafc48753758c749fe7cf5ba479997bb
Abbey Masters <abbeykmasters@gmail.com>
2019-09-26 16:07:55 -0700

    reviewer tests get all and get by id

--- e2e/__tests__/reviewer.test.js
+++ e2e/__tests__/reviewer.test.js
@@ -12,17 +12,61 @@ describe('reviewers api', () => {
     company: `Joe's All American Pie and Movie Review Shack`
   };
 
-  it('post a reviewer', () => {
+  function postReviewer(reviewer) {
     return request
       .post('/api/reviewers')
-      .send(data)
+      .send(reviewer)
       .expect(200)
-      .then(({ body }) => {
-        expect(body).toEqual({
+      .then(({ body }) => body);
+  }
+
+  it('post a reviewer', () => {
+    return postReviewer(data)
+      .then(reviewer => {
+        expect(reviewer).toEqual({
           _id: expect.any(String),
           __v: 0,
           ...data
         });
+      });
+  });
+
+  it('gets all reviewers', () => {
+    return Promise.all([
+      postReviewer({ name: 'joe', company: 'joe co' }),
+      postReviewer({ name: 'sam', company: 'sam co' }),
+    ])
+      .then(() => {
+        return request 
+          .get('/api/reviewers')
+          .expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.length).toBe(2);
+        expect(body[0]).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          name: 'joe',
+          company: 'joe co'
+        });
+      });
+  });
+
+
+  it('gets reviewer by id', () => {
+    return postReviewer(data)
+      .then(reviewer => {
+        console.log(reviewer);
+        return request
+          .get(`/api/reviewers/${reviewer._id}`)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body).toEqual({
+              _id: expect.any(String),
+              __v: 0,
+              ...data
+            });
+          });
       });
   });
 
