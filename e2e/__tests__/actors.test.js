509daa3900ba72d67950192b250e3d67c5471cf7
Joe Klause <joeklause@gmail.com>
2019-09-30 11:44:34 -0700

    fixed get by id actor

--- e2e/__tests__/actors.test.js
+++ e2e/__tests__/actors.test.js
@@ -2,17 +2,9 @@ const request = require('../request');
 const db = require('../db');
 
 describe('actor api', () => {
-
   beforeEach(() => {
     return db.dropCollection('actors');
   });
-
-
-  const actor = {
-    name: 'joe',
-    dob: new Date('4/20/1860'),
-    pob: 'Springfield'
-  };
 
   function postActor(actor) {
     return request
@@ -24,16 +16,62 @@ describe('actor api', () => {
       });
   }
 
-  it('posts an actor', () => {
-    return postActor(actor)
-      .then(actor => {
-        expect(actor).toEqual({
-          _id: expect.any(String),
-          __v: 0,
-          ...actor
-        });
-      });
+  function postFilm(film) {
+    return request
+      .post('/api/actors')
+      .send(actor)
+      .expect(200)
+      .then(({ body }) => {
+        film.cast[0].actor = body._id;
+        return request
+          .post('/api/studios')
+          .send(studio)
+          .expect(200);
+      })
+      .then(({ body }) => {
+        film.studio = body._id;
+        return request
+          .post('/api/films')
+          .send(film)
+          .expect(200);
+      })
+      .then(({ body }) => body);
+  }
 
+  const actor = {
+    name: 'joe',
+    dob: new Date('4/20/1860'),
+    pob: 'Springfield'
+  };
+
+  const film = {
+    title: 'Some bad movie',
+    studio: '',
+    released: 1969,
+    cast: [
+      {
+        role: 'Billy'
+      }
+    ]
+  };
+
+  const studio = {
+    name: 'Creepy Hollywood Studio Inc',
+    address: {
+      city: 'HollyLand',
+      state: 'Calirofnia',
+      country: 'Merica'
+    }
+  };
+
+  it('posts an actor', () => {
+    return postActor(actor).then(actor => {
+      expect(actor).toEqual({
+        _id: expect.any(String),
+        __v: 0,
+        ...actor
+      });
+    });
   });
 
   it('gets all actors', () => {
@@ -43,98 +81,64 @@ describe('actor api', () => {
       postActor({ name: 'newthing', dob: new Date('4/20/1860'), pob: 'hello' })
     ])
       .then(() => {
-        return request
-          .get('/api/actors')
-          .expect(200);
+        return request.get('/api/actors').expect(200);
       })
       .then(({ body }) => {
         expect(body.length).toBe(3);
       });
   });
 
-  // - this test passes, but not in travis
-
-  // it('gets actor by id', () => {
-  //   return postActor(actor)
-  //     .then(returnedActor => {
-  //       return request
-  //         .get(`/api/actors/${returnedActor._id}`)
-  //         .expect(200);
-  //     })
-  //     .then(({ body }) => {
-  //       expect(body).toEqual({
-  //         __v: 0,
-  //         _id: expect.any(String),
-  //         dob: '1860-04-20T07:52:58.000Z',
-  //         name: 'joe',
-  //         pob: 'Springfield',
-  //       });
-  //     });
-  // });
+  it('gets actor by id', () => {
+    return postActor(actor).then(returnedActor => {
+      film.cast[0] = {
+        _id: returnedActor._id
+      };
+      return postFilm(film)
+        .then(() => {
+          return request.get(`/api/actors/${returnedActor._id}`).expect(200);
+        })
+        .then(({ body }) => {
+          expect(body).toMatchInlineSnapshot(
+            {
+              _id: expect.any(String),
+              films: [
+                {
+                  _id: expect.any(String)
+                }
+              ]
+            },
+            `
+            Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "dob": "1860-04-20T07:52:58.000Z",
+              "films": Array [
+                Object {
+                  "_id": Any<String>,
+                  "released": 1969,
+                  "title": "Some bad movie",
+                },
+              ],
+              "name": "joe",
+              "pob": "Springfield",
+            }
+          `
+          );
+        });
+    });
+  });
 
   it('deletes an actor', () => {
-    return postActor(actor)
-      .then(actor => {
-        return request.delete(`/api/actors/${actor._id}`)
-          .expect(200);
-      });
+    return postActor(actor).then(actor => {
+      return request.delete(`/api/actors/${actor._id}`).expect(200);
+    });
   });
 
   it('will not delete an actor in a movie', () => {
-
-    const actor = {
-      name: 'That Guy',
-      dob: new Date('4/20/1969'),
-      pob: 'Springfield'
-    };
-
-    const studio = {
-      name: 'Creepy Hollywood Studio Inc',
-      address: {
-        city: 'HollyLand',
-        state: 'Calirofnia',
-        country: 'Merica'
-      }
-    };
-
-    const film = {
-      title: 'Some bad movie',
-      studio: '',
-      released: 1969,
-      cast: [
-        {
-          role: 'Billy'
-        }
-      ]
-    };
-
-    function postFilm(film) {
+    return postFilm(film).then(returnedFilm => {
       return request
-        .post('/api/actors')
-        .send(actor)
-        .expect(200)
-        .then(({ body }) => {
-          film.cast[0].actor = body._id;
-          return request
-            .post('/api/studios')
-            .send(studio)
-            .expect(200);
-        })
-        .then(({ body }) => {
-          film.studio = body._id;
-          return request
-            .post('/api/films')
-            .send(film)
-            .expect(200);
-        })
-        .then(({ body }) => body);
-    }
-
-
-    return postFilm(film)
-      .then(returnedFilm => {
-        return request.delete(`/api/actors/${returnedFilm.cast[0].actor}`)
-          .expect(400);
-      });
+        .delete(`/api/actors/${returnedFilm.cast[0].actor}`)
+        .expect(400);
+    });
   });
 });
